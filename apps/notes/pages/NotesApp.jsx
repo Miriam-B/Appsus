d77f5eeafa2e144012b7ec1b1c0df3b1f44d0b34
Miriam Baranovska <maryam1649@gmail.com>
2021-05-08 21:43:42 +0300

    This is the final pushdooooooown

--- apps/notes/pages/NotesApp.jsx
+++ apps/notes/pages/NotesApp.jsx
@@ -1,5 +1,6 @@
 export function NotesApp() {
    return <section className="container">
-        <img className="construction-img" src="../../../assets/img/construction.jpeg" alt="unfinished"></img>
+        <img className="construction-img" src="../../../assets/img/IMG_2998.PNG" alt="unfinished"></img>
+        <p>Hanging out with my cat. Will come back later to work on this page.</p>
     </section>
 }
